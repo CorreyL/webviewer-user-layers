825a1a9a7448ec5826f15aeaf142113709efe1b3
Correy Lim <correylim@gmail.com>
2021-11-24 16:02:55 -0800

    Add annotation summary to last page
    This is a really bad commit, re-commit in `main` later

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React, { useRef, useEffect, useState } from 'react';
-import WebViewer from '@pdftron/webviewer';
+import WebViewer, { Core } from '@pdftron/webviewer';
 import LayerOptions from './LayerOptions/LayerOptions';
 import './App.css';
+import html2canvas from 'html2canvas';
+
+function base64ToBlob(base64) {
+  const binaryString = window.atob(base64);
+  const len = binaryString.length;
+  const bytes = new Uint8Array(len);
+  for (let i = 0; i < len; ++i) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+
+  return new Blob([bytes], { type: 'application/pdf' });
+};
 
 const App = () => {
   const viewer = useRef(null);
@@ -19,15 +31,180 @@ const App = () => {
       },
       viewer.current,
     ).then((instance) => {
-      const { documentViewer } = instance.Core;
+      const { documentViewer, annotationManager } = instance.Core;
 
       documentViewer.addEventListener('documentLoaded', () => {
         setWvInstance(instance);
       });
 
-      documentViewer.addEventListener('annotationsLoaded', () => {
+        const getNoteIcon = annotation => {
+          const noteIcon = document.createElement('span');
+          noteIcon.className = 'dot';
+          const { R, G, B } = annotation.Color;
+          noteIcon.style['background-color'] = `rgb(${R}, ${G}, ${B})`;
+          return noteIcon;
+        };
+
+        function getRotationRad(pageNumber) {
+          const orientation = documentViewer.getRotation(pageNumber);
+          return (4 - orientation) * (Math.PI / 2);
+        }
+
+        function getDocumentCenter(pageNumber) {
+          let result;
+          if (pageNumber <= documentViewer.getPageCount()) {
+            result = documentViewer.getDocument().getPageInfo(pageNumber);
+          } else {
+            result = {
+              width: 0,
+              height: 0,
+            };
+          }
+          return { x: result.width / 2, y: result.height / 2 };
+        }
+
+        const rotateRad = (cx, cy, x, y, radians) => {
+          const s = Math.sin(radians);
+          const c = Math.cos(radians);
+        
+          const nx = (c * (x - cx)) + (s * (y - cy)) + cx; // xcos(theta) + ysin(theta)
+          const ny = (c * (y - cy)) - (s * (x - cx)) + cy; // -ysin(theta) + xcos(theta)
+        
+          return { x: nx, y: ny };
+        };
+
+        const getNoteInfo = (annotation, dateFormat, language) => {
+          const info = document.createElement('div');
+          let date = annotation.DateCreated.toLocaleDateString();
+          // let date = dayjs(annotation.DateCreated).format(dateFormat);
+        
+          // if (language) {
+          //   date = dayjs(annotation.DateCreated).locale(language).format(dateFormat);
+          // }
+        
+          info.className = 'note__info';
+          if (annotation.Subject === '' || annotation.Subject === null || annotation.Subject === undefined) {
+            info.innerHTML = `
+              Author: ${annotationManager.getDisplayAuthor(annotation['Author']) || ''} &nbsp;&nbsp;
+              Date: ${date}
+            `;
+          } else {
+            info.innerHTML = `
+              Author: ${annotationManager.getDisplayAuthor(annotation['Author']) || ''} &nbsp;&nbsp;
+              Subject: ${annotation.Subject} &nbsp;&nbsp;
+              Date: ${date}
+            `;
+          }
+        
+          return info;
+        };
+        
+        const getNoteContent = annotation => {
+          const contentElement = document.createElement('div');
+          const contentText = annotation.getContents();
+        
+          contentElement.className = 'note__content';
+          if (contentText) {
+            // ensure that new lines are preserved and rendered properly
+            contentElement.style.whiteSpace = 'pre-wrap';
+            contentElement.innerHTML = `${contentText}`;
+          }
+          return contentElement;
+        };
+      
+        const getNote = (annotation, dateFormat, language) => {
+          const note = document.createElement('div');
+          note.className = 'note';
+        
+          const noteRoot = document.createElement('div');
+          noteRoot.className = 'note__root';
+        
+          const noteRootInfo = document.createElement('div');
+          noteRootInfo.className = 'note__info--with-icon';
+          const noteIcon = getNoteIcon(annotation);
+
+          noteRootInfo.appendChild(noteIcon);
+          noteRootInfo.appendChild(getNoteInfo(annotation, dateFormat, language));
+          noteRoot.appendChild(noteRootInfo);
+          noteRoot.appendChild(getNoteContent(annotation));
+        
+          note.appendChild(noteRoot);
+          annotation.getReplies().forEach(reply => {
+            const noteReply = document.createElement('div');
+            noteReply.className = 'note__reply';
+            noteReply.appendChild(getNoteInfo(reply, dateFormat, language));
+            noteReply.appendChild(getNoteContent(reply));
+        
+            note.appendChild(noteReply);
+          });
+        
+          return note;
+        };
+
+      documentViewer.addEventListener('annotationsLoaded', async () => {
         setAnnotationsLoaded(true);
-      });
+        const printableAnnotationNotes = annotationManager.getAnnotationsList()
+          .filter(
+            annotation =>
+              annotation.Listable &&
+              // annotation.PageNumber === pageNumber &&
+              !annotation.isReply() &&
+              !annotation.isGrouped() &&
+              annotation.Printable,
+          );
+        const sortedNotes = printableAnnotationNotes.sort((a, b) => {
+          if (a.PageNumber === b.PageNumber) {
+            const rotation = getRotationRad(a.PageNumber);
+            const center = getDocumentCenter(a.PageNumber);
+    
+            // Simulated with respect to the document origin
+            const rotatedA = [
+              rotateRad(center.x, center.y, a.X, a.Y, rotation),
+              rotateRad(center.x, center.y, a.X + a.Width, a.Y + a.Height, rotation),
+            ];
+            const rotatedB = [
+              rotateRad(center.x, center.y, b.X, b.Y, rotation),
+              rotateRad(center.x, center.y, b.X + b.Width, b.Y + b.Height, rotation),
+            ];
+    
+            const smallestA = rotatedA.reduce(
+              (smallest, current) => (current.y < smallest ? current.y : smallest),
+              Number.MAX_SAFE_INTEGER,
+            );
+            const smallestB = rotatedB.reduce(
+              (smallest, current) => (current.y < smallest ? current.y : smallest),
+              Number.MAX_SAFE_INTEGER,
+            );
+    
+            return smallestA - smallestB;
+          }
+          return a.PageNumber - b.PageNumber;
+        });
+        const container = document.createElement('div');
+        container.className = 'page__container';
+    
+        const header = document.createElement('div');
+        header.className = 'page__header';
+    
+        /**
+         * @todo Re-implement the header for each page
+         */
+        // container.appendChild(header);
+        sortedNotes.forEach(annotation => {
+          header.innerHTML = `Page ${annotation.PageNumber}`;
+          const note = getNote(annotation, 'YYYY/MM/DD', 'English');
+    
+          container.appendChild(note);
+        });
+        document.body.appendChild(container);
+        const canvasToUse = await html2canvas(container);
+        container.remove();
+        const data = canvasToUse.toDataURL("image/jpeg", 1.0);
+        const documentToMerge = await instance.Core.createDocument(base64ToBlob(data.split(',')[1]), { extension: 'jpg' });
+        const doc = documentViewer.getDocument();
+        const pageIndexToInsert = doc.getPageCount() + 1;
+        doc.insertPages(documentToMerge, [1], pageIndexToInsert);
+      }, { once: true });
 
       instance.UI.annotationPopup.add({
         dataElement: 'copy-for-ownership',
